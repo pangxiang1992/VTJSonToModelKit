e4655f28dfd470da7894c44a908787ba052a3f00
庞翔 <pangxiangios@163.com>
2018-04-11 09:42:15 +0800

    Initial Commit

---
+++ VTJSonToModelKit/VTJSonToModelKit.h
@@ -0,0 +1,19 @@
+//
+//  VTJSonToModelKit.h
+//  VTJSonToModelKit
+//
+//  Created by pangxiang on 4/11/18.
+//  Copyright © 2018 pangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTJSonToModelKit.
+FOUNDATION_EXPORT double VTJSonToModelKitVersionNumber;
+
+//! Project version string for VTJSonToModelKit.
+FOUNDATION_EXPORT const unsigned char VTJSonToModelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTJSonToModelKit/PublicHeader.h>
+
+
